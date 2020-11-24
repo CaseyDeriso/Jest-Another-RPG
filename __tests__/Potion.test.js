2fc37ebbe0e25f1cb7c136296a1a4c4214bae7f5
Casey <caseyderiso@gmail.com>
2020-11-23 23:06:24 -0600

    added methods to get stats and inventory on player object constructor

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,16 +1,17 @@
-const Potion = require('../lib/Potion.js');
+const Player = require("../lib/Player.js");
+const Potion = require("../lib/Potion.js");
 
-test('creates a health potion object with name and value', () => {
-    const potion= new Potion('health');
+test("creates a health potion object with name and value", () => {
+  const potion = new Potion("health");
 
-    expect(potion.name).toBe('health');
-    expect(potion.value).toEqual(expect.any(Number));
+  expect(potion.name).toBe("health");
+  expect(potion.value).toEqual(expect.any(Number));
 });
 
-test('creates a random potion object', () => {
-    const potion = new Potion();
+test("creates a random potion object", () => {
+  const potion = new Potion();
 
-    expect(potion.name).toEqual(expect.any(String));
-    expect(potion.name.length).toBeGreaterThan(0);
-    expect(potion.value).toEqual(expect.any(Number));
-})
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
+  expect(potion.value).toEqual(expect.any(Number));
+});
